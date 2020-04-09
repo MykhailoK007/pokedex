bdbbdce099bc39b44674be944edd49f3536593d7
MykhailoK007 <kotmykhailo2000@gmail.com>
2020-04-09 23:59:43 +0300

    Code refactoring

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import store from "./redux/store";
 import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(<Provider store={store}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App/>
+    </BrowserRouter>
 </Provider> ,document.getElementById('root'));
 
