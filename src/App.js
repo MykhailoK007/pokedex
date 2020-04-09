7f16b00c338fade4d4ab5f8ffcd25b2de02b961b
MykhailoK007 <kotmykhailo2000@gmail.com>
2020-04-10 00:29:16 +0300

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import CardsContainer from "./Components/Cards/CardsContainer";
+import {Route} from "react-router";
 
 function App() {
 
   return (
         <div>
-          <h1 className="header">Pokedex</h1>
+                <h1 className="header">Pokedex</h1>
                 <CardsContainer/>
+
         </div>
 
   );
