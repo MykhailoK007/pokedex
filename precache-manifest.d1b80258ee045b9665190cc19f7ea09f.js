b4d5640380e3424f22693497cd684e81748b683a
MykhailoK007 <kotmykhailo2000@gmail.com>
2020-04-09 23:44:01 +0300

    Updates

---
+++ precache-manifest.d1b80258ee045b9665190cc19f7ea09f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd80957e0ab0c220b0ecdc9061e74ae",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "082dd04c64108ae737c7",
+    "url": "/pokedex/static/css/main.4e2ccc72.chunk.css"
+  },
+  {
+    "revision": "81bdffb4a7f65d25d23e",
+    "url": "/pokedex/static/js/2.f7f04ae1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pokedex/static/js/2.f7f04ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082dd04c64108ae737c7",
+    "url": "/pokedex/static/js/main.fea0d764.chunk.js"
+  },
+  {
+    "revision": "9ad62cfa2b3463e027e8",
+    "url": "/pokedex/static/js/runtime-main.d9a2a7a5.js"
+  }
+]);
